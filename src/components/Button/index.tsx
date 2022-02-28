e1d7dd96a15af45c31d3be0f7afd0ea5f9fca0b4
Eduardo Pinheiro <3dup1n@gmail.com>
2022-02-28 20:59:20 -0300

    fix: button  component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -26,22 +26,23 @@ export default function Button(props: Props) {
 }
 
 function ButtonBase(props: Props) {
+  const { withIcon, ...rest } = props;
   return (
     <ChakraButton
-      {...props}
-      colorScheme={props.colorScheme || "blue"}
-      w={props.w ?? props.fit ? "fit-content" : "auto"}
+      colorScheme="blue"
+      w={props.fit ? "fit-content" : "auto"}
       d="flex"
       alignItems="center"
       justifyContent="center"
+      {...rest}
     >
       <Text as="span" whiteSpace="nowrap">
         {props.children}
       </Text>
-      {props.withIcon && props.withIcon === true ? (
+      {withIcon && withIcon === true ? (
         <Icon ml={4} as={BsArrowRight} />
       ) : (
-        props.withIcon
+        withIcon
       )}
     </ChakraButton>
   );
